60d307db0e63d57e3ca206a169a5d147df12fc7d
Caique Mendes <90288443+caique405@users.noreply.github.com>
2022-11-22 13:01:26 -0300

    Update Tartaruga.py

--- Tartaruga.py
+++ Tartaruga.py
@@ -17,3 +17,41 @@ setposition(100, 100) #estabelece coodenadas x e y
 setheading(45) #altera o angulo da seta
 
 done()
+
+
+
+
+
+# from turtle import *
+# from random import choice
+# corCaneta = ('Yellow', 'Red', 'Blue', 'Purple', 'Pink')
+# for c in range(0, 4):
+#     pencolor(choice(corCaneta))
+#     forward(90)
+#     right(90)
+#     pencolor(choice(corCaneta))
+
+# setposition(65, 65)
+
+# for c in range(0, 4):
+#     pencolor(choice(corCaneta))
+#     forward(90)
+#     right(90)
+#     pencolor(choice(corCaneta))
+# forward(90)
+# setposition(90, 0)
+# right(90)
+# forward(90)
+# done()
+
+
+# from turtle import *
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
+# end_fill()
+# done()
